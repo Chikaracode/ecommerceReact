09e68cbdd856a888610712c705aafc67c75043dd
Naomi Dezar <chikara.dev97@gmail.com>
2023-03-28 17:08:02 -0500

    clase 3

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Navbar from './Navbar';
+import Home from './Home'
+import Footter from './Footter';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+      <Footter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+    return <h1>Estoy en el Home</h1>
+}
+
+export default Home
